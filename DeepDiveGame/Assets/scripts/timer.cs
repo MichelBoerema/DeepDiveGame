def8ddf49eb26f008c6406f3e5835eb2585141a1
roelsiemens <roelsiemens@gmail.com>
2024-05-16 11:08:08 +0200

    r

--- DeepDiveGame/Assets/scripts/timer.cs
+++ DeepDiveGame/Assets/scripts/timer.cs
@@ -10,6 +10,9 @@ public class timer : MonoBehaviour
     private bool starttime = false;
     float besttime = 1000;
     float lasttime;
+    float seconds;
+    float minutes;
+    float millisecs;
     public TMPro.TextMeshProUGUI Ltime;
     public TMPro.TextMeshProUGUI Ltimelast;
     public TMPro.TextMeshProUGUI LtimeBest;
@@ -26,7 +29,7 @@ public class timer : MonoBehaviour
         if (other.gameObject.tag == "Finish")
         {
             lasttime = timelap;
-            Ltimelast.text = "Last: " + lasttime.ToString("f3");
+            Ltimelast.text = string.Format("Last: " + "{0:00}:{1:00}:{2:000}", minutes, seconds, millisecs);
         }
         if (other.gameObject.tag == "Finish")
         {
@@ -39,13 +42,17 @@ public class timer : MonoBehaviour
         if (starttime)
         {
             timelap = timelap +Time.deltaTime;
-            //Debug.Log(timelap);
-            Ltime.text = "Time: " + timelap.ToString("f3");
+
+            minutes = Mathf.FloorToInt(timelap / 60);
+            seconds = Mathf.FloorToInt(timelap % 60);
+            millisecs = (timelap % 1) * 1000;
+
+            Ltime.text = string.Format("{0:00}:{1:00}:{2:000}",minutes,seconds,millisecs);
         }
         if (lasttime < besttime && lapcount > 1)
         {
             besttime = lasttime;
-            LtimeBest.text = "Best: " + besttime.ToString("f3");
+            LtimeBest.text = string.Format("Best: " + "{0:00}:{1:00}:{2:000}", minutes, seconds, millisecs);
         }
 
         laps.text = "lap: " + lapcount.ToString() + "/3 ";
